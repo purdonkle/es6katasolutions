bf85daf176f2d62c38b5cfdea495eb4a59a013e0
purdonkle <adampurdondev@gmail.com>
2020-11-14 08:07:25 +0200

    katas 47 - 58

---
+++ reflect/basics.js
@@ -0,0 +1,34 @@
+// 58: Reflect - basics
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Reflect` basics', function() {
+    describe('Reflect is special, it is different to e.g. `Object`', function() {
+      it('it`s of type object', function() {
+        const actualType = typeof Reflect;
+        assert.equal(actualType, typeof Reflect);
+      });
+      it('it can not be instantiated (`new Reflect()`)', function() {
+        const tryToConstruct = () => { new Reflect; };
+        assert.throws(tryToConstruct, TypeError);
+      });
+      it('has no `call` method (as opposed to e.g. Object)', function() {
+        const actual = 'undefined';
+        assert.equal(actual, typeof Reflect.call);
+      });
+    });
+    
+    describe('some `Reflect` usages', function() {
+      it('`Reflect.construct()` is like `new ClassName`', function() {
+        let Class = () => {};
+        assert.equal(Reflect.construct(Class, []) instanceof Class, true);
+      });
+      it('`Reflect.get()` returns a property`s value', function() {
+        let obj = {x: 23};
+        assert.equal(Reflect.get(obj, 'x'), 23);
+      });
+      it('`Reflect.has()` is like `in` just as a function', function() {
+        let obj = {'x': 1};
+        assert.equal(Reflect.has(obj, 'x'), true);
+      });
+    });
+  });
